fe3ad8c64953660e83b17a5224809b97c91cfcbf
larsmay <larsmay@iu.edu>
2019-02-11 16:14:57 -0500

    Initial

---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const {dialogflow} = require('actions-on-google');
+
+const bodyMeas = require('firebase-functions');
+
+const app = dialogflow({debug:true});
+
+app.intent("bodyMeas", (conv, {weight, height, type, gender, age}) => {
+
+    var message;
+    let bodyMass = bmi(weight, height, type);
+    let bodyFat = body_fat(bodyMass, gender, age);
+
+    message = `Your bmi is ${bodyMass} and your body fat percent is ${bodyFat}.`;
+
+    conv.close(message);
+
+});
+
+bmi = (weight, height, type) => {
+    var bmi;
+    if (type == 'metric'){
+        bmi = weight / (height * height);
+    }
+    else {
+        weight = weight * 0.45;
+        height = height * 0.025;
+        bmi = weight / (height * height);
+    }
+    return bmi;
+};
+
+body_fat =(bmi, gender, age) => {
+    var bodyPercent;
+
+    if (gender == "male"){
+        bodyPercent = (1.20*bmi)+(0.23 * age) - 16.2;
+    }
+
+    else {
+        bodyPercent = (1.20*bmi)+(0.23 * age) - 5.4;
+    }
+    return bodyPercent;
+};
+
+exports.generateBodyMeas = bodyMeas.https.onRequest(app);
