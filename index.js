f9084e4b84241f4803f4839dcbdf84aa10ec7ac6
larsmay <larsmay@iu.edu>
2019-02-20 13:57:13 -0500

    Update

--- index.js
+++ index.js
@@ -6,13 +6,18 @@ const bodyMeas = require('firebase-functions');
 
 const app = dialogflow({debug:true});
 
-app.intent("bodyMeas", (conv, {weight, height, type, gender, age}) => {
+app.intent("heart-Rate", (conv, {age, rest}) => {
 
     var message;
-    let bodyMass = bmi(weight, height, type);
-    let bodyFat = body_fat(bodyMass, gender, age);
-
-    message = `Your bmi is ${bodyMass} and your body fat percent is ${bodyFat}.`;
+    conv.data.age = age;
+    conv.data.rest = rest;
+    var max = 220 - age - rest;
+    
+    var heart1 = max * 0.6 + parseInt(rest);
+    var heart2 = max * 0.7 + parseInt(rest);
+    var heart3 = max * 0.8 + parseInt(rest);
+    
+    var message = `Your Zone 1 range is ${heart1} to ${heart2}. Your Zone 2 range is ${heart2} to ${heart3}. Your Zone 3 range is ${heart3} and greater`;
 
     conv.close(message);
 
@@ -20,20 +25,26 @@ app.intent("bodyMeas", (conv, {weight, height, type, gender, age}) => {
 
 app.intent("bmi", (conv) = (weight, height, type) => {
     var bmi;
+    conv.data.weight = weight;
+    conv.data.height = height;
+    conv.data.type = type;
     if (type == 'metric'){
         bmi = weight / (height * height);
+        conv.data.bmi = bmi;
     }
     else {
         weight = weight * 0.45;
         height = height * 0.025;
         bmi = weight / (height * height);
+        conv.data.bmi = bmi;
     }
     return bmi;
 });
 
 app.intent("body_fat", (conv) = (bmi, gender, age) => {
     var bodyPercent;
-
+    conv.data.gender = gender;
+    conv.data.age = age;
     if (gender == "male"){
         bodyPercent = (1.20*bmi)+(0.23 * age) - 16.2;
     }
