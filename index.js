a767f52128db78b723d946be8845337b296c6c2e
larsmay <larsmay@iu.edu>
2019-02-20 15:27:46 -0500

    Update

--- index.js
+++ index.js
@@ -17,16 +17,19 @@ app.intent("heart-Rate", (conv, {age, rest}) => {
     var heart2 = max * 0.7 + parseInt(rest);
     var heart3 = max * 0.8 + parseInt(rest);
     
-    var message = `Your Zone 1 range is ${heart1} to ${heart2}. Your Zone 2 range is ${heart2} to ${heart3}. Your Zone 3 range is ${heart3} and greater`;
+    message = `Your Zone 1 range is ${heart1} to ${heart2}. Your Zone 2 range is ${heart2} to ${heart3}. Your Zone 3 range is ${heart3} and greater. Would you like to know anything else?`;
 
-    conv.close(message);
+    conv.ask(message);
 
 });
 
-app.intent("bmi", (conv) = (weight, height, type) => {
+app.intent("bmi", (conv) = () => {
     var bmi;
-    conv.data.weight = weight;
-    conv.data.height = height;
+    var message;
+    console.log("I need height and weight");
+    let weight = conv.data.weight;
+    
+    let height = conv.data.height;
     conv.data.type = type;
     if (type == 'metric'){
         bmi = weight / (height * height);
@@ -38,11 +41,15 @@ app.intent("bmi", (conv) = (weight, height, type) => {
         bmi = weight / (height * height);
         conv.data.bmi = bmi;
     }
-    return bmi;
+    message = `Your bmi is ${bmi}. Would you like to know anything else?`;
+
+    conv.ask(message);
+    
 });
 
 app.intent("body_fat", (conv) = (bmi, gender, age) => {
     var bodyPercent;
+    var message;
     conv.data.gender = gender;
     conv.data.age = age;
     if (gender == "male"){
@@ -52,7 +59,9 @@ app.intent("body_fat", (conv) = (bmi, gender, age) => {
     else {
         bodyPercent = (1.20*bmi)+(0.23 * age) - 5.4;
     }
-    return bodyPercent;
+    message = `Your Body Fat percentage is ${bodyPercent} percent.  Would you like to know anything else?`;
+
+    conv.ask(message);
 });
 
 exports.generateBodyMeas = bodyMeas.https.onRequest(app);
