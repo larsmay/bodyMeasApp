2eef6514c8f2d5fbfed5ea550dc93229c256935d
larsmay <larsmay@iu.edu>
2019-02-25 15:22:41 -0500

    I think I got it

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyMeas = require('firebase-functions');
 
 const app = dialogflow({debug:true});
 
-app.intent("heart-Rate", (conv) => {
+app.intent("userRequestsHeart", (conv) => {
 
     var message;
     let age = conv.parameters['age'];
@@ -32,12 +32,28 @@ app.intent("heart-Rate", (conv) => {
 
         conv.ask(message);
 
-        conv.data.age = age;
-        conv.data.rest = rest;
-
     }
     
 
+});
+
+app.intent("heart-rate",(conv) => {
+    var message;
+    let age = conv.parameters['age'];
+    conv.data.age = age;
+
+    let rest = conv.parameters['rest'];
+    conv.data.rest = rest;
+
+    var max = 220 - age - rest;
+    
+    var heart1 = max * 0.6 + parseInt(rest);
+    var heart2 = max * 0.7 + parseInt(rest);
+    var heart3 = max * 0.8 + parseInt(rest);
+    
+    message = `Your Zone 1 range is ${heart1} to ${heart2}. Your Zone 2 range is ${heart2} to ${heart3}. Your Zone 3 range is ${heart3} and greater. Would you like to know anything else?`;
+
+    conv.ask(message);
 });
 
 app.intent("userRequestsBMI", (conv) => {
@@ -68,7 +84,7 @@ app.intent("userRequestsBMI", (conv) => {
 
             
         }
-        message = `Your bmi is ${bmi}. Would you like to know anything else?`;
+        message = `Your bmi is ${bmi}, based on previous submitted values. Would you like to know anything else?`;
 
         conv.ask(message);
     }
@@ -104,7 +120,7 @@ app.intent("bmi",(conv) => {
         conv.ask(message);    
 });
 
-app.intent("body_fat", (conv) => {
+app.intent("userRequestsBodyFat", (conv) => {
     var bodyPercent;
     var message;
     let gender = conv.data.gender;
@@ -130,4 +146,24 @@ app.intent("body_fat", (conv) => {
     
 });
 
+    app.intent("body_fat", (conv) => {
+        let bmi = conv.data.bmi;
+        let gender = conv.parameters['gender'];
+        conv.data.gender = gender;
+        let age = conv.parameters['age'];
+        conv.data.age = age;
+        var bodyPercent;
+        
+        if (gender == "male"){
+            bodyPercent = (1.20*bmi)+(0.23 * age) - 16.2;
+        }
+
+        else {
+            bodyPercent = (1.20*bmi)+(0.23 * age) - 5.4;
+        }
+
+        message = `Your Body Fat percentage is ${bodyPercent} percent.  Would you like to know anything else?`;
+
+        conv.ask(message);
+    });
 exports.generateBodyMeas = bodyMeas.https.onRequest(app);
