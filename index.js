e6a85b9fb17b97f8d0453867dd774d322067908f
larsmay <larsmay@iu.edu>
2019-02-25 16:01:57 -0500

    derpy

--- index.js
+++ index.js
@@ -22,8 +22,11 @@ app.intent("userRequestsHeart", (conv) => {
         var max = 220 - age - rest;
     
         var heart1 = max * 0.6 + parseInt(rest);
+        conv.data.heart1 = heart1;
         var heart2 = max * 0.7 + parseInt(rest);
+        conv.data.heart2 = heart2;
         var heart3 = max * 0.8 + parseInt(rest);
+        conv.data.heart3 = heart3;
     
         message = `Your Zone 1 range is ${heart1} to ${heart2}. Your Zone 2 range is ${heart2} to ${heart3}. Your Zone 3 range is ${heart3} and greater. Would you like to know anything else?`;
 
@@ -45,8 +48,11 @@ app.intent("heartRate",(conv) => {
     var max = 220 - age - rest;
     
     var heart1 = max * 0.6 + parseInt(rest);
+    conv.data.heart1 = heart1;
     var heart2 = max * 0.7 + parseInt(rest);
+    conv.data.heart2 = heart2;
     var heart3 = max * 0.8 + parseInt(rest);
+    conv.data.heart3 = heart3;
     
     message = `Your Zone 1 range is ${heart1} to ${heart2}. Your Zone 2 range is ${heart2} to ${heart3}. Your Zone 3 range is ${heart3} and greater. Would you like to know anything else?`;
 
@@ -130,10 +136,12 @@ app.intent("userRequestsBodyFat", (conv) => {
     else {
         if (gender == "male"){
             bodyPercent = (1.20*bmi)+(0.23 * age) - 16.2;
+            conv.data.bodyPercent = bodyPercent;
         }
 
         else {
             bodyPercent = (1.20*bmi)+(0.23 * age) - 5.4;
+            conv.data.bodyPercent = bodyPercent;
         }
         message = `Your Body Fat percentage is ${bodyPercent} percent.  Would you like to know anything else?`;
 
@@ -153,10 +161,12 @@ app.intent("userRequestsBodyFat", (conv) => {
 
         if (gender == "male"){
             bodyPercent = (1.20*bmi)+(0.23 * age) - 16.2;
+            conv.data.bodyPercent = bodyPercent;
         }
 
         else {
             bodyPercent = (1.20*bmi)+(0.23 * age) - 5.4;
+            conv.data.bodyPercent = bodyPercent;
         }
 
         var message = `Your Body Fat percentage is ${bodyPercent} percent.  Would you like to know anything else?`;
