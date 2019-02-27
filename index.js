3c1de2b9d02a047b20aa37835e7e050566dfb11f
larsmay <larsmay@iu.edu>
2019-02-27 12:23:27 -0500

    Update

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.intent("userRequestsHeart", (conv) => {
         var heart2 = max * 0.7 + parseInt(rest);
         var heart3 = max * 0.8 + parseInt(rest);
     
-        message = `Your Zone 1 range is ${heart1} to ${heart2}. Your Zone 2 range is ${heart2} to ${heart3}. Your Zone 3 range is ${heart3} and greater. Would you like to know anything else?`;
+        message = `Based on previously supplied values your Zone 1 range is ${heart1} to ${heart2}. Your Zone 2 range is ${heart2} to ${heart3}. Your Zone 3 range is ${heart3} and greater. Would you like to know your bmi or body fat percentage?`;
 
         conv.ask(message);
 
@@ -49,7 +49,7 @@ app.intent("heartRate",(conv) => {
     var heart2 = max * 0.7 + parseInt(rest);
     var heart3 = max * 0.8 + parseInt(rest);
     
-    message = `Your Zone 1 range is ${heart1} to ${heart2}. Your Zone 2 range is ${heart2} to ${heart3}. Your Zone 3 range is ${heart3} and greater. Would you like to know anything else?`;
+    message = `Your Zone 1 range is ${heart1} to ${heart2}. Your Zone 2 range is ${heart2} to ${heart3}. Your Zone 3 range is ${heart3} and greater. Would you like to know your bmi or body fat percentage?`;
 
     conv.ask(message);
 });
@@ -144,7 +144,7 @@ app.intent("userRequestsBodyFat", (conv) => {
     }
     else {
         bodyPercent = calcPercent(age,gender,bmi);
-        message = `Your Body Fat percentage is ${bodyPercent} percent.  Would you like to know anything else?`;
+        message = `Based on previously entered values your Body Fat percentage is ${bodyPercent} percent.  Would you like to know your bmi or target heart rate?`;
 
         conv.ask(message);
     }
@@ -160,7 +160,7 @@ app.intent("userRequestsBodyFat", (conv) => {
         conv.data.age = age;
         var bodyPercent = calcPercent(age,gender,bmi);
 
-        var message = `Your Body Fat percentage is ${bodyPercent} percent.  Would you like to know anything else?`;
+        var message = `Your Body Fat percentage is ${bodyPercent} percent.  Would you like to know your bmi or target heart rate?`;
 
         conv.ask(message);
     });
